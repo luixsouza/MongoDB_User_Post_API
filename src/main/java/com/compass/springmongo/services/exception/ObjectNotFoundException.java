73bce79b79b5cdbfd71a583882edbb00e8fb0feb
Luis Eduardo <luixst1@gmail.com>
2025-01-17 12:04:53 -0300

    Getting user by id

---
+++ src/main/java/com/compass/springmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.compass.springmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
