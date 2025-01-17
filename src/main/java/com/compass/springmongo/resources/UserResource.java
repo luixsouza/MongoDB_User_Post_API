9ad030352ca43df6d9af8a38415ee7f80535f4cc
Luis Eduardo <luixst1@gmail.com>
2025-01-17 18:28:33 -0300

    Endpoint to return a user's posts

--- src/main/java/com/compass/springmongo/resources/UserResource.java
+++ src/main/java/com/compass/springmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.compass.springmongo.domain.Post;
 import com.compass.springmongo.domain.User;
 import com.compass.springmongo.dto.UserDTO;
 import com.compass.springmongo.services.UserService;
@@ -61,5 +62,11 @@ public class UserResource {
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
         
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
