c0c5fdce6019b2191eccfdbb5b485ba16d6cb326
Luis Eduardo <luixst1@gmail.com>
2025-01-17 18:45:53 -0300

    Adding comments to posts

--- src/main/java/com/compass/springmongo/config/Instantiation.java
+++ src/main/java/com/compass/springmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.compass.springmongo.domain.Post;
 import com.compass.springmongo.domain.User;
 import com.compass.springmongo.dto.AuthorDTO;
+import com.compass.springmongo.dto.CommentDTO;
 import com.compass.springmongo.repository.PostRepository;
 import com.compass.springmongo.repository.UserRepository;
 
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu Viagem", "Vou viajar para Piri. Valeu!", new AuthorDTO (maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Tô feliz hj hahaha", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa Viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Lá é muito bom", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Aproveite!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
