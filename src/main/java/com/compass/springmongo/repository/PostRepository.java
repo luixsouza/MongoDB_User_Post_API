c2d39a7f45a2837a46ef82af1f1152a2533d7a03
Luis Eduardo <luixst1@gmail.com>
2025-01-17 13:55:37 -0300

    Creating entity Post and relating it to User

---
+++ src/main/java/com/compass/springmongo/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package com.compass.springmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.compass.springmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+
+}
