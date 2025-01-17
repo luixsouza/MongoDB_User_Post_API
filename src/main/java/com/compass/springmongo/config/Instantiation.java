7a2e6c67c731f10f02ef5fceac26575f9f5defca
Luis Eduardo <luixst1@gmail.com>
2025-01-17 14:13:17 -0300

    Projection of author data with DTO

--- src/main/java/com/compass/springmongo/config/Instantiation.java
+++ src/main/java/com/compass/springmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.compass.springmongo.domain.Post;
 import com.compass.springmongo.domain.User;
+import com.compass.springmongo.dto.AuthorDTO;
 import com.compass.springmongo.repository.PostRepository;
 import com.compass.springmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
 
-        Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu Viagem", "Vou viajar para Piri. Valeu!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Tô feliz hj hahaha", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu Viagem", "Vou viajar para Piri. Valeu!", new AuthorDTO (maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Tô feliz hj hahaha", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 
