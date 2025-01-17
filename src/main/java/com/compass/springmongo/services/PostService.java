6387e6f41640a82ba8a10ede3b9c0733666d2f40
Luis Eduardo <luixst1@gmail.com>
2025-01-17 18:35:17 -0300

    Getting post by id

---
+++ src/main/java/com/compass/springmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.compass.springmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.compass.springmongo.domain.Post;
+import com.compass.springmongo.repository.PostRepository;
+import com.compass.springmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+        }
+
+}
